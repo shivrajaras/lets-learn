9244049a5317340c9e137828fbce9c4b716dfdcf
Shivaraja Aras <shivarajaaras@Shivarajas-MacBook-Air.local>
2021-07-30 09:22:03 +0530

    first commit

---
+++ src/main/java/com/learning/Pen.java
@@ -0,0 +1,54 @@
+package com.learning;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * PEN object 
+ * 
+ * 
+ * atributes -> colour, size,length 
+ * actions ->   writing
+ * access specifiers -> public,package,protected,private
+ * 
+ * name convention -> start with  $,characher,_
+ * 
+ * 
+ * static and instance
+ */ 
+public class Pen {
+
+	// attributes
+	public String colour;
+	
+	public static int height=10;
+
+	public Pen(String colour) {
+		this.colour = colour;
+	}
+	
+    
+	
+    public void whichPen() {
+    	System.out.println(" I'm "+this.colour+ "Pen ");
+    }
+	
+	
+	public static void main(String[] args) {
+
+		Pen redPen = new Pen("Red");
+		Pen bluePen = new Pen("Blue");
+		
+		
+		System.out.println(redPen.height);
+		redPen.whichPen();
+		bluePen.whichPen();
+	}
+
+}
+
+
+
+
+
+
